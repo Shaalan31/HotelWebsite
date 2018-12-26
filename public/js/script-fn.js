2e519c6a1a3704f793a8cc0151539a330a4cb2fc
Unknown <samar.gamal144@gmail.com>
2018-12-26 02:56:48 +0200

    credit card validation

--- public/js/script-fn.js
+++ public/js/script-fn.js
@@ -82,20 +82,11 @@ function deleteRoom(newRoomName, e){
 function validateCC() {
     var ccNum = document.getElementById("credit_card").value;
     var visaRegEx = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
-    // var mastercardRegEx = /^(?:5[1-5][0-9]{14})$/;
-    // var amexpRegEx = /^(?:3[47][0-9]{13})$/;
-    // var discovRegEx = /^(?:6(?:011|5[0-9][0-9])[0-9]{12})$/;
     var isValid = false;
 
     if (visaRegEx.test(ccNum)) {
         isValid = true;
-    } //else if(mastercardRegEx.test(ccNum)) {
-    //     isValid = true;
-    // } else if(amexpRegEx.test(ccNum)) {
-    //     isValid = true;
-    // } else if(discovRegEx.test(ccNum)) {
-    //     isValid = true;
-    // }
+    }
 
     if(!isValid) {
         document.getElementById('credit_card').setCustomValidity("Please press enter a valid credit card number!");
